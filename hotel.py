c7d8bbbabf07295ad25dad97c9f6973e87e1dc63
Peter Mbolonzi <94831058+mbolonzi-design@users.noreply.github.com>
2022-04-21 16:41:18 +0300

    added a nested dictionary

--- hotel.py
+++ hotel.py
@@ -2,3 +2,18 @@ name = "John"
 room = 12
 print(name)
 print(room)
+
+#nested dict
+tourismkenya = {
+  "National Parks": {
+    "Name" : "Nairobi National Park",
+    "Location" : "Lang'ata",
+    "Fee" : 250
+  },
+  "Camps":{
+    "Location" : "Maasai Mara Reserve",
+    "Name" : "Loisaba Camp",
+    "Housing Type" : "Double bed",
+    "Fee" : 23500
+  }
+}
